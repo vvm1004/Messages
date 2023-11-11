5ca66e9544ebbcd1b483ebed638cf005189090df
vvm1004 <96302642+vvm1004@users.noreply.github.com>
2023-11-11 14:10:30 +0700

    Fix pagination bug

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,11 +1,18 @@
 const path = require('path');
 const express = require('express');
-const feedRoutes = require('./routers/feed');
-const authRoutes = require('./routers/auth');
+const fs = require('fs');
+
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer')
 const { v4: uuidv4 } = require('uuid');
+const {graphqlHTTP} = require('express-graphql')
+const graphqlSchema = require('./graphql/schema')
+const graphqlResolver = require('./graphql/resolvers')
+const auth = require('./middleware/auth')
+
+const { clearImage } = require('./util/file');
+
 
 const app = express();
 
@@ -47,13 +54,43 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.method === 'OPTIONS'){
+      return res.sendStatus(200);
+    }
     next();
 });
 
+app.use(auth);
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated!');
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: 'No file provided!' });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+    .status(201)
+    .json({ message: 'File stored.', filePath: req.file.path });
+});
 
+app.use('/graphql', graphqlHTTP({
+  schema: graphqlSchema,
+  rootValue: graphqlResolver,
+  graphiql: true,
+  customFormatErrorFn(err) {
+    if (!err.originalError) {
+      return err;
+    }
+    const data = err.originalError.data;
+    const message = err.message || 'An error occurred';
+    const code = err.originalError.code || 500;
+    return { message, status: code, data };
+  }
+}));
 
 app.use((error, req, res, next) => {
   console.log(error);
@@ -66,11 +103,7 @@ app.use((error, req, res, next) => {
 mongoose.connect(MONGODB_URL)
   // mongoose.connect("mongodb://127.0.0.1:27017/network")
   .then(result => {
-    const server = app.listen(8080);
-    const io = require('./socket').init(server);
-    io.on('connection', socket => {
-      console.log('CLinet connected');
-    })
+    app.listen(8080);
   })
   .catch(err => {
     console.log(err)
